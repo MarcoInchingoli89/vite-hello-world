05a10f1243d83f55e9fc9fd658b22e5847506bfc
Marco Inchingoli <evilmarkus@hotmail.it>
2022-11-09 15:47:57 +0100

    Add AppTitle Component

---
+++ vite-hello-world/src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+<script></script>
+
+<template>
+
+    <div class="container">
+        <h1 class="title">La mia prima app con Vite</h1>
+    </div>
+
+
+</template>
+
+<style scoped>
+.container {
+    height: 100vh;
+}
+
+.title {
+    text-align: center;
+    color: red;
+    padding-top: 0;
+}
+</style>
